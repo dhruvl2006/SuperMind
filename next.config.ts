78f61300fdae8d04d30ec73d6934535326908a6f
Aditya Bavadekar <bvkraditya@gmail.com>
2025-01-09 23:25:09 +0530

    Fixed tsconfig.json and added env in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    FLOW_ID: process.env.FLOW_ID,
+    LANGFLOW_ID: process.env.LANGFLOW_ID,
+    APPLICATION_TOKEN: process.env.APPLICATION_TOKEN,
+  },
 };
 
 export default nextConfig;
