a6fb2dd3a8597881b5c2f2de2acb30d85a77f2ca
dhruvl2006 <lohar.dhruv07@gmail.com>
2025-01-10 09:21:49 +0530

    Updated chatcontext

--- app/api/gen/route.ts
+++ app/api/gen/route.ts
@@ -1,122 +1,125 @@
 import Message from "@/models/message.modle";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request:NextRequest){
-    const body = await request.json();
-    const { userId, conversationId, userMessage } = body;
-    if (!userId){
-        return NextResponse.json({ message: 'User ID is required!' }, { status: 400 });
-    }
-    if (!conversationId){
-        return NextResponse.json({ message: 'Conversation ID is required!' }, { status: 400 });
-    }
-    if (!userMessage){
-        return NextResponse.json({ message: 'Message is required!' }, { status: 400 });
-    }
-    new Message({
-        userId,
-        conversationId,
-        sender: "user",
-        text: userMessage,
-    }).save();
-    const analysis = await generateAnalysis(userMessage);
-    var messageId = "";
-    if (!analysis.error){
-        const message = new Message({
-            userId,
-            conversationId,
-            sender: "bot",
-            text: analysis.text,
-            stats: analysis.stats,
-        });
-        await message.save();
-    }
-    const statusCode = analysis.error ? 500 : 200;
-    return NextResponse.json({
-        ...analysis,
-        _id: messageId,
-        conversationId,
-    }, { status: statusCode });
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { userId, conversationId, userMessage } = body;
+  if (!userId) {
+    return NextResponse.json(
+      { message: "User ID is required!" },
+      { status: 400 }
+    );
+  }
+  if (!conversationId) {
+    return NextResponse.json(
+      { message: "Conversation ID is required!" },
+      { status: 400 }
+    );
+  }
+  if (!userMessage) {
+    return NextResponse.json(
+      { message: "Message is required!" },
+      { status: 400 }
+    );
+  }
+  new Message({
+    userId,
+    conversationId,
+    sender: "user",
+    text: userMessage,
+  }).save();
+  const analysis = await generateAnalysis(userMessage);
+  var messageId = "";
+  if (!analysis.error) {
+    const message = new Message({
+      userId,
+      conversationId,
+      sender: "bot",
+      text: analysis.text,
+      stats: analysis.stats,
+    });
+    await message.save();
+  }
+  const statusCode = analysis.error ? 500 : 200;
+  return NextResponse.json(
+    {
+      ...analysis,
+      _id: messageId,
+      conversationId,
+    },
+    { status: statusCode }
+  );
 }
 
-const generateAnalysis = async (inputValue:string) => {
+const generateAnalysis = async (inputValue: string) => {
+  const stream = false;
+  const endpoint = `/lf/${process.env.LANGFLOW_ID}/api/v1/run/${process.env.FLOW_ID}?stream=${stream}`;
+  const baseURL = "https://api.langflow.astra.datastax.com";
+  const url = `${baseURL}${endpoint}`;
+  const tweaks = {
+    "ChatInput-KxWBy": {},
+    "ChatOutput-U8492": {},
+    "Prompt-voioz": {},
+    "Agent-dodTI": {},
+    "GoogleGenerativeAIModel-1cQoY": {},
+    "AstraDBToolComponent-i8Tw8": {},
+  };
+  const body = {
+    input_value: inputValue,
+    input_type: "text",
+    output_type: "chat",
+    tweaks: tweaks,
+    sender: "client-user",
+    sender_name: "client-user",
+  };
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.APPLICATION_TOKEN}`,
+  };
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(body),
+    });
 
+    const responseContent = await response.json();
+    if (!response.ok) {
+      throw new Error(
+        `${response.status} ${response.statusText} - ${JSON.stringify(
+          responseContent
+        )}`
+      );
+    }
+    const responseMessage =
+      responseContent.outputs[0].outputs[0].results.message;
+    const stats_blocks = responseMessage.content_blocks[0].contents[1];
+    var likes: number[] = [],
+      shares: number[] = [],
+      comments: number[] = [],
+      avg_sentiment_scores: number[] = [];
+    stats_blocks.output.forEach((block: any) => {
+      likes.push(block.data.likes);
+      shares.push(block.data.shares);
+      comments.push(block.data.comments);
+      avg_sentiment_scores.push(block.data.sentiment_score);
+    });
     return {
-        error: false,
-        text: "This is a sample response from the bot",
-        post_type: "text",
-        stats: {
-            likes: [0],
-            shares: [0],
-            comments: [0],
-            avg_sentiment_score: [0],
-        },
-    }
-
-    const stream = false;
-    const endpoint = `/lf/${process.env.LANGFLOW_ID}/api/v1/run/${process.env.FLOW_ID}?stream=${stream}`;
-    const baseURL = "https://api.langflow.astra.datastax.com";
-    const url = `${baseURL}${endpoint}`;
-    const tweaks = {
-        "ChatInput-KxWBy": {},
-        "ChatOutput-U8492": {},
-        "Prompt-voioz": {},
-        "Agent-dodTI": {},
-        "GoogleGenerativeAIModel-1cQoY": {},
-        "AstraDBToolComponent-i8Tw8": {},
+      error: false,
+      text: responseMessage.text,
+      post_type: stats_blocks.tool_input.post_type,
+      stats: {
+        likes: likes,
+        shares: shares,
+        comments: comments,
+        avg_sentiment_score: avg_sentiment_scores,
+      },
     };
-    const body = {
-        input_value: inputValue,
-        input_type: "text",
-        output_type: "chat",
-        tweaks: tweaks,
-        sender: "client-user",
-        sender_name: "client-user",
+  } catch (error: any) {
+    console.error("Request Error:", error.message);
+    return {
+      error: true,
+      text: error.message ? error.message : "Something went wrong",
     };
-    const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.APPLICATION_TOKEN}`,
-    };
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(body),
-        });
-
-        const responseContent = await response.json();
-        if (!response.ok) {
-            throw new Error(
-                `${response.status} ${response.statusText} - ${JSON.stringify(
-                    responseContent
-                )}`
-            );
-        }
-        const responseMessage = responseContent.outputs[0].outputs[0].results.message;
-        const stats_blocks = responseMessage.content_blocks[0].contents[1];
-        var likes : number[] = [], shares: number[] = [], comments: number[] = [], avg_sentiment_scores: number[] = [];
-        stats_blocks.output.forEach((block:any) => {
-            likes.push(block.data.likes);
-            shares.push(block.data.shares);
-            comments.push(block.data.comments);
-            avg_sentiment_scores.push(block.data.sentiment_score);
-        });
-        return {
-            error: false,
-            text: responseMessage.text,
-            post_type: stats_blocks.tool_input.post_type,
-            stats: {
-                likes: likes,
-                shares: shares,
-                comments: comments,
-                avg_sentiment_score: avg_sentiment_scores,
-            },
-        };
-    } catch (error:any) {
-        console.error("Request Error:", error.message);
-        return {
-            error: true,
-            text: error.message ? error.message : "Something went wrong",
-        };
-    }
+  }
 };
--- helper/db.ts
+++ helper/db.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-import { exit } from 'process';
+import mongoose from "mongoose";
+import { exit } from "process";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log('Connected to database');
-    } catch (error) {
-        console.error('Error connecting to database', error);
-        exit(-1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Error connecting to database", error);
+    exit(-1);
+  }
+};
